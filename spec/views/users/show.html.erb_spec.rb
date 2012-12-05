32aa2f64208dfe53df1df359fc8eb0e0f20be517
georgevolkov <georgevolkov@gmail.com>
2012-12-04 23:08:01 -0300

    Users

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "users/show.html.erb" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :name => "Name",
+      :password => "Password",
+      :email => "Email"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Password/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Email/)
+  end
+end
