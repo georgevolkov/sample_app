1ad6c0930cd2775d424d0c32a966ea577af0a354
georgevolkov <georgevolkov@gmail.com>
2013-01-09 17:23:04 -0300

    Before Pagination

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authentificate(params[:session][:email], params[:session][:password])
-
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
     if user.nil?
       flash.now[:error] = "Invalid email/password combination."
       @title = "Sign in"
       render 'new'
+    else
+      sign_in user
+      redirect_to user
     end
-  else
-    sign_in user
-    redirect_to user
   end
 
   def destroy
